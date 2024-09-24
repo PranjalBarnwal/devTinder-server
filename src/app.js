f8679967fccb99a2f792559abe41b5be9df6dea2
Pranjal Barnwal <happybarnwal007@gmail.com>
2024-09-25 04:14:26 +0530

    routing revision done✅

--- src/app.js
+++ src/app.js
@@ -2,14 +2,27 @@ const express= require("express");
 
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from home");
+
+app.get("/user/:random1/:random2",(req,res)=>{
+    const query=req.query;
+    res.send({
+        ...query,
+        ...req.params,
+        fName:"Happy",
+        lName:"Kumar"
+    })
 })
+
+app.post("/user",(req,res)=>{
+    res.send("Data saved successfully");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully");
+})
+
 app.use("/test",(req,res)=>{
     res.send("Hello from server");
-})
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello");
 })
 
 app.listen(3000,()=>console.log("Server is successfully listening to port 3000")
